9ad2cc9d11e45dde8b52e7112bf7658565b0b350
Jean-Joooo <124114074+Jean-Joooo@users.noreply.github.com>
2024-01-08 17:19:54 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,22 +1,21 @@
+"""server.py"""
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
-app = Flask("Emotion Detection")
+app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def sent_detector():
     """Function detector"""
-    text_to_detect = request.args.get('textToAnalyze')
-    response = emotion_detector(text_to_detect)
-    formated_response = emotion_predictor(response)
-    if formated_response['dominant_emotion'] is None:
-        return "Invalid text! Please try again."
-    return (
-        f"For the given statement, the system response is 'anger': {formated_response['anger']} "
-        f"'disgust': {formated_response['disgust']}, 'fear': {formated_response['fear']}, "
-        f"'joy': {formated_response['joy']} and 'sadness': {formated_response['sadness']}. "
-        f"The dominant emotion is {formated_response['dominant_emotion']}."
-    )
+    text_to_analyze = request.args.get('textToAnalyze')
+    response = emotion_detector(text_to_analyze)
+    if     response ['dominant_emotion']['emotion'] is None :
+        response_text = "invalid text, please try again!"
+    else:
+        response_text =f"For the given statement,the system response is:{response['emotion']}. \n" \
+                        f"The dominant emotion is : {response['dominant_emotion']}."
+    return response_text
 
 @app.route("/")
 def render_index_page():
